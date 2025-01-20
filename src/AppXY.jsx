dfd2eb929ecada3a4f592be6dde78f30472d8451
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-21 03:17:06 +0900

    Step2. 객체로 상태 관리
    - 서로 연관성 존재 → 객체로 상태 관리

--- src/AppXY.jsx
+++ src/AppXY.jsx
@@ -3,17 +3,15 @@ import './AppXY.css';
 
 export default function AppXY() {
 
-  const [x, setX] = useState(0);
-  const [y, setY] = useState(0);
+  const [position, setPosition] = useState({ x: 0, y: 0 });
 
   const handlePointerMove = (e) => {
-    setX(e.clientX);
-    setY(e.clientY);
-  }
+    setPosition({ x: e.clientX, y: e.clientY });
+  };
 
   return (
     <div className='container' onPointerMove={handlePointerMove}>
-      <div className='pointer' style={{transform: `translate(${x}px, ${y}px)`}}/>
+      <div className='pointer' style={{transform: `translate(${position.x}px, ${position.y}px)`}}/>
     </div>
   );
 }
