f63e66e872a8d2992d6429744211ae1adff7ca82
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-21 03:41:31 +0900

    Step2. setState Callback 사용
    - 이전 값을 바탕으로 값을 변경하는 상황에서는 Callback을 사용하는 것이 더 안전하다.

--- src/AppMentor.jsx
+++ src/AppMentor.jsx
@@ -20,7 +20,7 @@ export default function AppMentor(props) {
       <button
         onClick={() => {
           const name = prompt(`what's your mentor's name?`);
-          setPerson({...person, mentor: {...person.mentor, name: name}});
+          setPerson((person) => ({...person, mentor: {...person.mentor, name: name}}));
         }}
       >
         멘토 이름 바꾸기
@@ -28,7 +28,7 @@ export default function AppMentor(props) {
       <button
         onClick={() => {
           const title = prompt(`what's your mentor's title?`);
-          setPerson({...person, mentor: {...person.mentor, title: title}});
+          setPerson((person) => ({...person, mentor: {...person.mentor, title: title}}));
         }}
       >
         멘토 타이틀 바꾸기
