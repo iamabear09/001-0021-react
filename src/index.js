5c8dd87822b7a3c83a680c97c2e83fa64efe40ff
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-21 03:31:50 +0900

    Project Setting

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import AppMentor from './AppMentor';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <AppMentor />
   </React.StrictMode>
 );
 
