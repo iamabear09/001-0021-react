472b2158a0788fedadeb7456f20806e889fcfbe2
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-21 17:19:04 +0900

    Done

--- src/AppForm.jsx
+++ src/AppForm.jsx
@@ -1,14 +1,14 @@
 import React, { useState } from 'react';
 
 export default function AppForm() {
-  const [form, setFrom] = useState({ name: '', email: '' });
+  const [form, setForm] = useState({ name: '', email: '' });
   const handleSubmit = (e) => {
-    e.preventDefault();
+    e.preventDefault(); // Submit의 경우 Browser 기본 event 방지
     console.log(form);
   };
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setFrom({ ...form, [name]: value });
+    setForm({ ...form, [name]: value });  // name(변수)을 key 로 사용하는 방법
   };
   return (
     <form onSubmit={handleSubmit}>
