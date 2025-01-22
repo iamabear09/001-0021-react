2d654d1cbb3a71ed923460aec26cb212bc513529
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-22 21:16:26 +0900

    Step4. 로딩/에러 추가 [내 방식]

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -3,19 +3,34 @@ import React, { useEffect, useState } from 'react';
 export default function Products() {
   const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
+
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+
   const handleChange = () => setChecked((prev) => !prev);
 
   useEffect(() => {
+    setLoading(true);
+
     fetch(`data/${checked ? 'sale_' : ''}products.json`)
       .then((res) => res.json())
       .then((data) => {
         console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
+        setLoading(false);
         setProducts(data);
+      })
+      .catch((err) => {
+        setError(true);
+        setLoading(false);
       });
+
     return () => {
       console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
     };
   }, [checked]);
+
+  if (loading) return (<div>laoding...</div>);
+  if (error) return (<div>error 발생...</div>)
 
   return (
     <>
