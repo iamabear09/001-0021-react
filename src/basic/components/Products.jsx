9c544b073c810c508cff8835cee18ff19c4564e9
Jonghun, Park <jhp2014.dev@gmail.com>
2025-01-23 13:33:25 +0900

    Step3. Custom Hooks 생성
    - 네트워크에서 Data Fetch 및 활용하는 로직은 여러 컴포넌트에서 활용 될 수 있다.
    - 즉, 로직이 재사용 될 수 있으므로, 커스텀 훅으로 만들어서 재사용 가능하도록 한다.

--- src/basic/components/Products.jsx
+++ src/basic/components/Products.jsx
@@ -1,31 +1,10 @@
 import React, { useEffect, useState } from 'react';
+import useProducts from "../hooks/use-products";
 
 export default function Products() {
-  const [products, setProducts] = useState([]);
   const [checked, setChecked] = useState(false);
-
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState(undefined);
-
+  const [loading, error, products] = useProducts({salesOnly: checked});
   const handleChange = () => setChecked((prev) => !prev);
-
-  useEffect(() => {
-    setLoading(true);
-    setError(undefined);
-
-    fetch(`data/${checked ? 'sale_' : ''}products.json`)
-      .then((res) => res.json())
-      .then((data) => {
-        console.log('🔥뜨끈한 데이터를 네트워크에서 받아옴');
-        setProducts(data);
-      })
-      .catch((err) => setError(`에러가 발생헀음!...: ${err}`))
-      .finally(() => setLoading(false));
-
-    return () => {
-      console.log('🧹 깨끗하게 청소하는 일들을 합니다.');
-    };
-  }, [checked]);
 
   if (loading) return (<div>loading...</div>);
   if (error) return (<div>{error}</div>)
